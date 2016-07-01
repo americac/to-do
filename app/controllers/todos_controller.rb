324b1bd48bcf9990af8b00c946708a529587b70b
America Calleros <america.quintero@gmail.com>
2016-07-01 15:47:02 -0500

    Test ability to add todo and verify it is displayed on todos/index.html view

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,8 +1,21 @@
 class TodosController < ApplicationController
+  def new
+    @todo = Todo.new
+  end
+
   def index
+    @todos = Todo.all
   end
 
   def create
-    render :show
+    @todo = Todo.new(todo_params)
+    @todo.save
+    @todos = Todo.all
+    render :index
+  end
+
+  private
+  def todo_params
+    params.require(:todo).permit(:description)
   end
 end
--- spec/features/user_creates_todo_spec.rb
+++ spec/features/user_creates_todo_spec.rb
@@ -3,8 +3,9 @@ require "rails_helper"
 feature "User creates todo" do
   scenario "Successfully" do
     visit root_path
-    fill_in "todo", with: "New Todo"
+    fill_in "Description", with: "New Todo"
     click_on "Save"
     expect(page).to have_content('All todos')
+    expect(page).to have_content('New Todo')
   end
 end
