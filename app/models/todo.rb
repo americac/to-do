f2824d75138bbd40d3693abbde4029219071d7eb
America Calleros <america.quintero@gmail.com>
2016-07-05 15:03:32 -0500

    Allow todos to be marked incomplete

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -6,4 +6,9 @@ class Todo < ActiveRecord::Base
   def completed!
     touch :completed_at
   end
+
+  def mark_incomplete!
+    update_column :completed_at, nil
+  end
+
 end
--- spec/features/user_marks_todo_incomplete_spec.rb
+++ spec/features/user_marks_todo_incomplete_spec.rb
@@ -2,12 +2,12 @@ require "rails_helper"
 
 feature "User marks todo incomplete" do
   scenario "successfully" do
-    #sign_in
-    #create_todo "Buy milk"
-    #click_on "Mark complete"
-    #click_on "Mark incomplete"
+    sign_in
+    create_todo "Buy milk"
+    click_on "Mark complete"
+    click_on "Mark incomplete"
 
-    #expect(page).not_to display_completed_todo "Buy milk"
-    #expect(page).to display_todo "Buy Milk"
+    expect(page).not_to display_completed_todo "Buy milk"
+    expect(page).to display_todo "Buy milk"
   end
 end
